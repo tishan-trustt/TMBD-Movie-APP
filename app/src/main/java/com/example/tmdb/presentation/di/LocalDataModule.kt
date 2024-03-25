2e81775a3154c2551ed75d0da60cc394a4e8d966
Tishan <tishan.koleh@trustt.com>
2024-03-25 18:42:17 +0530

    LocalDataModules

---
+++ app/src/main/java/com/example/tmdb/presentation/di/LocalDataModule.kt
@@ -0,0 +1,35 @@
+package com.example.tmdb.presentation.di
+
+import com.example.tmdb.data.repository.movie.Artist.ArtistLocalDataSource
+import com.example.tmdb.data.repository.movie.Artist.ArtistLocalDataSourceImpl
+import com.example.tmdb.data.repository.movie.MovieLocalDataSource
+import com.example.tmdb.data.repository.movie.MovieLocalDataSourceImpl
+import com.example.tmdb.data.repository.movie.TVshow.TvShowLocalDataSource
+import com.example.tmdb.data.repository.movie.TVshow.TvShowLocalDataSourceImpl
+import com.example.tmdb.roomDB.ArtistDAO
+import com.example.tmdb.roomDB.MovieDao
+import com.example.tmdb.roomDB.TvshowsDAO
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class LocalDataModule {
+    @Singleton
+    @Provides
+    fun provideMovieLocalDataSource(movieDao: MovieDao):MovieLocalDataSource{
+        return MovieLocalDataSourceImpl(movieDao)
+    }
+
+    @Singleton
+    @Provides
+    fun provideArtistLocalDataSource(artistDAO: ArtistDAO):ArtistLocalDataSource{
+        return ArtistLocalDataSourceImpl(artistDAO)
+    }
+
+    @Singleton
+    @Provides
+    fun provideTvShowLocalDataSource(tvShowDao:TvshowsDAO):TvShowLocalDataSource{
+        return TvShowLocalDataSourceImpl(tvShowDao)
+    }
+}
