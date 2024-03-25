1671fd076937a1d1d0ca9bb1b5e0faf33e60e0d7
Tishan <tishan.koleh@trustt.com>
2024-03-25 18:45:16 +0530

    CachedDataModules

---
+++ app/src/main/java/com/example/tmdb/presentation/di/CachedDataModule.kt
@@ -0,0 +1,33 @@
+package com.example.tmdb.presentation.di
+
+import com.example.tmdb.data.repository.movie.Artist.ArtistCachedDataSource
+import com.example.tmdb.data.repository.movie.Artist.ArtistCachedDataSourceImpl
+import com.example.tmdb.data.repository.movie.MovieCacheDataSource
+import com.example.tmdb.data.repository.movie.MovieCachedDataSourceImpl
+import com.example.tmdb.data.repository.movie.TVshow.TvShowCachedDataSource
+import com.example.tmdb.data.repository.movie.TVshow.TvShowCachedDataSourceImpl
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class CachedDataModule {
+
+    @Singleton
+    @Provides
+    fun provideMovieCacheDataSource():MovieCacheDataSource{
+        return MovieCachedDataSourceImpl()
+    }
+
+    @Singleton
+    @Provides
+    fun provideArtistCacheDataSource():ArtistCachedDataSource{
+        return ArtistCachedDataSourceImpl()
+    }
+
+    @Singleton
+    @Provides
+    fun provideTvShowCacheDataSource():TvShowCachedDataSource{
+        return TvShowCachedDataSourceImpl()
+    }
+}
